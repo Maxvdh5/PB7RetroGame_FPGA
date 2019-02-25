913e78be241f7921841c7af5a55777828271e519
Pelle van Gils <pelle@pellevangils.nl>
2019-02-25 19:36:08 +0100

    update blockdesign IP's after bitstream

---
+++ src/blockdesign/ip/blockdesign_clk_wiz_0_0/blockdesign_clk_wiz_0_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+-- Date        : Mon Feb 25 19:22:50 2019
+-- Host        : xilinux running 64-bit Ubuntu 18.04.2 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /media/sf_shared/PB7RetroGame_FPGA/src/blockdesign/ip/blockdesign_clk_wiz_0_0/blockdesign_clk_wiz_0_0_stub.vhdl
+-- Design      : blockdesign_clk_wiz_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity blockdesign_clk_wiz_0_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end blockdesign_clk_wiz_0_0;
+
+architecture stub of blockdesign_clk_wiz_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,reset,locked,clk_in1";
+begin
+end;
