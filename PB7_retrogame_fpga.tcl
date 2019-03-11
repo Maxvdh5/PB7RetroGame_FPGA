4b958e9d273d740e2d91177b03149be527fbb088
Pelle van Gils <pelle@pellevangils.nl>
2019-03-11 19:35:43 +0100

    fix IO constraints file

--- PB7_retrogame_fpga.tcl
+++ PB7_retrogame_fpga.tcl
@@ -140,9 +140,9 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/../PROGH/digilent-xdc/Basys-3-Master.xdc"]"
+set file "[file normalize "$origin_dir/src/constraints/io.xdc"]"
 set file_added [add_files -norecurse -fileset $obj $file]
-set file "$origin_dir/../PROGH/digilent-xdc/Basys-3-Master.xdc"
+set file "$origin_dir/src/constraints/io.xdc"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
---
+++ src/constraints/io.xdc
@@ -0,0 +1,16 @@
+# Pmod Header JA
+# Sch name = JA1
+# 'Controller' input
+
+#Sch name = JA3
+set_property PACKAGE_PIN J2 [get_ports {JA[0]}]
+	set_property IOSTANDARD LVCMOS33 [get_ports {JA[0]}]
+#Sch name = JA4
+set_property PACKAGE_PIN G2 [get_ports {JA[1]}]
+	set_property IOSTANDARD LVCMOS33 [get_ports {JA[1]}]
+#Sch name = JA9
+set_property PACKAGE_PIN H2 [get_ports {JA[2]}]
+	set_property IOSTANDARD LVCMOS33 [get_ports {JA[2]}]
+#Sch name = JA10
+set_property PACKAGE_PIN G3 [get_ports {JA[3]}]
+	set_property IOSTANDARD LVCMOS33 [get_ports {JA[3]}]
